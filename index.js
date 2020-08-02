4ba122a8f39adcc988878a5eeeb4e170ce29d6da
bengrunfeld <benjamin.grunfeld@gmail.com>
2020-08-02 19:58:59 +0300

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+// const mongodb = require("mongodb");
+// const config = require("./data");
+
+const app = express();
+const PORT = 4000;
+// const client = mongodb.MongoClient;
+
+// client.connect(config.DB, { useNewUrlParser: true }, (err, db) => {
+//   if (err) {
+//     console.log("database is not connected");
+//   } else {
+//     console.log("connected!!");
+//   }
+// });
+
+app.get("/", (req, res) => {
+  res.json("I love docker!");
+});
+
+app.listen(PORT, () => {
+  console.log("Your server is running on PORT:", PORT);
+});
